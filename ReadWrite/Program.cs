909bc1beee8018ef06b160ad7e560c07ef78de25
Martin Kröner <kronerm9@gmail.com>
2021-01-22 07:41:59 +0100

    ReadWrite: take path from args

--- ReadWrite/Program.cs
+++ ReadWrite/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using HKX2;
 using Tests;
@@ -8,26 +7,12 @@ namespace ReadWrite
 {
     internal static class Program
     {
-        private static void Main()
+        private static void Main(string[] args)
         {
-            var rootPath =
-                Path.GetFullPath(Path.Combine(
-                    AppDomain.CurrentDomain.BaseDirectory ?? throw new InvalidOperationException(), "../../../../"));
-            var testFilesPath = Path.Combine(rootPath, "UnitTests", "Testfiles", "BotW");
-            var wiiu = Path.Combine(testFilesPath, "WiiU");
-            var nx = Path.Combine(testFilesPath, "Switch");
-
-            var path = $@"{wiiu}/Physics/Cloth/UMii_Hylia_Hair_B_002/UMii_Hylia_Hair_B_002_C1.hkcl";
-            var extension = path.Split('.').Last();
-
-            var outPath = Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
-                path.Split('/').Last());
-
-            var hkxFiles = TestBase.ReadBotwHKX(path);
-            var outBytes = TestBase.WriteBotwHKX(hkxFiles, extension, HKXHeader.BotwNx());
+            var roots = TestBase.ReadBotwHKX(args[0]);
+            var outBytes = TestBase.WriteBotwHKX(roots, args[0].Split('.').Last(), HKXHeader.BotwNx());
             
-            File.WriteAllBytes(outPath, outBytes);
+            File.WriteAllBytes(args[0] + ".out", outBytes);
         }
     }
 }
