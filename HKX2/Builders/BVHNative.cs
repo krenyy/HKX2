2e1dc20d1ef3d66c917b46a9c4cc791c3cd61b87
Martin Kröner <kronerm9@gmail.com>
2021-02-15 06:47:57 +0100

    Make NativeBVHNode struct and its members readonly

--- HKX2/Builders/BVHNative.cs
+++ HKX2/Builders/BVHNative.cs
@@ -4,16 +4,16 @@ using System.Runtime.InteropServices;
 namespace HKX2.Builders
 {
     [StructLayout(LayoutKind.Sequential)]
-    public struct NativeBVHNode
+    public readonly struct NativeBVHNode
     {
-        public float minX;
-        public float maxX;
-        public float minY;
-        public float maxY;
-        public float minZ;
-        public float maxZ;
-        public uint primitiveCount;
-        public uint firstChildOrPrimitive;
+        public readonly float minX;
+        public readonly float maxX;
+        public readonly float minY;
+        public readonly float maxY;
+        public readonly float minZ;
+        public readonly float maxZ;
+        public readonly uint primitiveCount;
+        public readonly uint firstChildOrPrimitive;
         
         public bool isLeaf => primitiveCount > 0;
     }
